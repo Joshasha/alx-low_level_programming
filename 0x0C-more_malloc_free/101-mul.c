114bf749b2ca9ac32ef0b3e656e78a5e6e62b69a
Joshasha <ashairejoshua@gmail.com>
2023-03-17 22:17:44 -0700

    Code corrected

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,14 +1,14 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include <stdlib.#h>
+#include <stidio.h>
 #include <ctype.h>
 
 /**
- * _is_zero - determines if any number is zero
- * @argv: argument vector.
- *
- * Return: no return.
- */
+* _is_zero - determines if any number is zero
+* @argv: argument vector.
+*
+* Return: no return.
+*/
 void _is_zero(char *argv[])
 {
 	int i, isn1 = 1, isn2 = 1;
@@ -19,14 +19,12 @@ void _is_zero(char *argv[])
 			isn1 = 0;
 			break;
 		}
-
 	for (i = 0; argv[2][i]; i++)
 		if (argv[2][i] != '0')
 		{
 			isn2 = 0;
 			break;
 		}
-
 	if (isn1 == 1 || isn2 == 1)
 	{
 		printf("0\n");
@@ -35,12 +33,12 @@ void _is_zero(char *argv[])
 }
 
 /**
- * _initialize_array - set memery to zero in a new array
- * @ar: char array.
- * @lar: length of the char array.
- *
- * Return: pointer of a char array.
- */
+* _initialize_array - set memery to zero in a new array
+* @ar: char array.
+* @lar: length of the char array.
+*
+* Return: pointer of a char array.
+*/
 char *_initialize_array(char *ar, int lar)
 {
 	int i = 0;
@@ -52,13 +50,13 @@ char *_initialize_array(char *ar, int lar)
 }
 
 /**
- * _checknum - determines length of the number
- * and checks if number is in base 10.
- * @argv: arguments vector.
- * @n: row of the array.
- *
- * Return: length of the number.
- */
+* _checknum - determines length of the number
+* and checks if number is in base 10.
+* @argv: arguments vector.
+* @n: row of the array.
+*
+* of the number.
+*/
 int _checknum(char *argv[], int n)
 {
 	int ln;
@@ -69,18 +67,17 @@ int _checknum(char *argv[], int n)
 			printf("Error\n");
 			exit(98);
 		}
-
 	return (ln);
 }
 
 /**
- * main - Entry point.
- * program that multiplies two positive numbers.
- * @argc: number of arguments.
- * @argv: arguments vector.
- *
- * Return: 0 - success.
- */
+* main - Entry point.
+* program that multiplies two positive numbers.
+* @argc: number of arguments.
+* @argv: arguments vector.
+*
+* Return: 0 - success.
+*/
 int main(int argc, char *argv[])
 {
 	int ln1, ln2, lnout, add, addl, i, j, k, ca;
